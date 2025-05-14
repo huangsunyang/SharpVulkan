a276e7d68eb3a407ed57521ef1c5ac029700ec0e
hansen <hansen@bytedance.com>
2025-05-14 20:36:42 +0800

    init my project

---
+++ SharpVulkanEngine/Program.cs
@@ -0,0 +1,51 @@
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+unsafe class MainWindow
+{
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+
+	private IWindow? window;
+
+	public static void Main()
+	{
+		var app = new MainWindow();
+		app.Run();
+	}
+
+	public void Run()
+	{
+		InitWindow();
+		MainLoop();
+		CleanUp();
+	}
+
+	private void InitWindow()
+	{
+		//Create a window.
+		var options = WindowOptions.DefaultVulkan with
+		{
+			Size = new Vector2D<int>(WIDTH, HEIGHT),
+			Title = "Vulkan"
+		};
+
+		window = Window.Create(options);
+		window.Initialize();
+
+		if (window.VkSurface is null)
+		{
+			throw new Exception("Windowing platform doesn't support Vulkan.");
+		}
+	}
+
+	private void MainLoop()
+	{
+		window!.Run();
+	}
+
+	private void CleanUp()
+	{
+		window?.Dispose();
+	}
+}
---
+++ SharpVulkanEngine/VulkanDevice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Silk.NET.Vulkan;
+using Silk.NET.Core;
+using Silk.NET.Windowing;
+
+namespace SharpVulkanEngine
+{
+	public unsafe class VulkanDevice
+	{
+		public Vk Api => Vk.GetApi();
+		protected Instance instance;
+		public VulkanDevice(IWindow window)
+		{
+			var appInfo = new ApplicationInfo()
+			{
+				SType = StructureType.ApplicationInfo,
+				PApplicationName = (byte*)Marshal.StringToHGlobalAnsi("Vulkan"),
+				PEngineName = (byte*)Marshal.StringToHGlobalAnsi("No Engine"),
+				EngineVersion = new Version32(1, 0, 0),
+				ApiVersion = Vk.Version13
+			};
+
+			var createInfo = new InstanceCreateInfo()
+			{
+				SType = StructureType.InstanceCreateInfo,
+				PApplicationInfo = &appInfo
+			};
+			
+			var extensions = window.VkSurface!.GetRequiredExtensions(out var extCount);
+			createInfo.EnabledExtensionCount = extCount;
+			createInfo.PpEnabledExtensionNames = extensions;
+		}
+	}
+}
