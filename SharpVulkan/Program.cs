ca42be7cb7b0aa3d8a2126fdd1138724ff8fdf7b
hansen <hansen@bytedance.com>
2025-05-14 16:37:08 +0800

    intial commit

---
+++ SharpVulkan/Program.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using System.Reflection;
+
+namespace ImGuiVulkan
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			new ImGuiVulkanApplication().Run();
+        }
+
+        internal static byte[] LoadEmbeddedResourceBytes(string path)
+		{
+            var name = typeof(Program).Assembly.GetName().Name;
+            using (var s = typeof(Program).Assembly.GetManifestResourceStream(name + "." + path))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    s.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
